f63f199663a77a71b85b1c0f42b3b5bfa8d6dcd9
Anant Majhi <anmajhi7@gmail.com>
2024-05-24 09:08:20 +0530

    Custom Stack partial

--- src/stack/StackExample.java
+++ src/stack/StackExample.java
@@ -12,6 +12,9 @@ public class StackExample {
         stack.push(3);
         stack.push(4);
         stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
 
         int popped = (int) stack.pop();
 
